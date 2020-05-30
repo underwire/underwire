c2e6a832c289b58162d2a191f73af9d9cae04fc2
Jeremy Darling <jeremy.darling@gmail.com>
2020-05-29 21:49:39 -0500

    Update to last version 1 of react-select

--- web/src/components/editors/nodeselect.js
+++ web/src/components/editors/nodeselect.js
@@ -1,58 +1,72 @@
 const React = require('react');
-const {
-  Component,
-} = React;
-const {
-  connect,
-} = require('react-redux');
-const Select = require('react-select');
+const { Component } = React;
+const { connect } = require('react-redux');
+import Select from 'react-select';
 
-class NodeSelect extends Component{
-  constructor(props){
+const getValueFrom = item => {
+  if (item.target && item.target.value) {
+    return item.target.value;
+  }
+  return (item && item.value) || undefined;
+};
+
+class NodeSelect extends Component {
+  constructor(props) {
     super();
-    this.state = {value: props.value};
+    this.state = { value: props.value };
   }
 
-  handleSelect(item){
-    const value = (item && item.value) || undefined;
-    this.setState({value});
+  handleSelect(item) {
+    const value = getValueFrom(item);
+    this.setState({ value });
     this.props.onSelected && this.props.onSelected(value);
   }
 
-  getValue(){
+  getValue() {
     return this.state.value;
   }
 
-  componentWillReceiveProps(props){
-    if(props.value){
-      this.setState({value: props.value});
+  componentWillReceiveProps(props) {
+    if (props.value) {
+      this.setState({ value: props.value });
     }
   }
 
-  render(){
-    const {
-      nodes,
-    } = this.props;
-    const value = this.state.value;
-    const items = nodes.map((node)=>{
-      const caption = node.version?`${node.name} (v${node.version})`:node.name;
+  render() {
+    const { nodes } = this.props;
+    const items = nodes.map(node => {
+      const caption = node.version
+        ? `${node.name} (v${node.version})`
+        : node.name;
       return {
         value: node.id,
-        label: caption,
+        label: caption
       };
     });
+    const value = this.state.value || (items[0] || {}).value;
+    console.log(items, value);
     return (
-      <Select ref="editor" options={items} value={value} onChange={this.handleSelect.bind(this)} />
+      <Select
+        ref="editor"
+        options={items}
+        value={value}
+        onChange={this.handleSelect.bind(this)}
+      />
     );
   }
-};
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
     value: ownProps.value,
     onSelected: ownProps.onSelected,
-    nodes: state.nodes,
+    nodes: state.nodes
   };
 };
 
-module.exports = connect(mapStateToProps, null, null, {withRef: true})(NodeSelect);
+module.exports = connect(
+  mapStateToProps,
+  null,
+  null,
+  { withRef: true }
+)(NodeSelect);
--- web/src/components/editors/select.js
+++ web/src/components/editors/select.js
@@ -1,45 +1,62 @@
 const React = require('react');
 
-class Select extends React.Component{
-  getValue(){
-    if(this.props.multiple){
-      var values = [].filter.call(this.refs.editor.options, function (o) {
-        return o.selected;
-      }).map(function (o) {
-        return o.value;
-      });
+class Select extends React.Component {
+  getValue() {
+    if (this.props.multiple) {
+      var values = [].filter
+        .call(this.refs.editor.options, function(o) {
+          return o.selected;
+        })
+        .map(function(o) {
+          return o.value;
+        });
       return values;
     }
     return this.refs.editor.value;
   }
 
-  render(){
-    const inputProps = Object.keys(this.props).reduce((obj, key)=>{
-      if(key === 'label'){
+  render() {
+    const inputProps = Object.keys(this.props).reduce((obj, key) => {
+      if (key === 'label') {
         return obj;
       }
-      if(key === 'value'){
-        obj.defaultValue = this.props.mapValues?this.props.mapValues(this.props.value):this.props.value;
+      if (key === 'value') {
+        obj.defaultValue = this.props.mapValues
+          ? this.props.mapValues(this.props.value)
+          : this.props.value;
         return obj;
       }
-      if(key === 'items'){
+      if (key === 'items') {
         return obj;
       }
       obj[key] = this.props[key];
       return obj;
     }, {});
-    const options = this.props.items.map((option)=>{
-      const isString = typeof(option)==='string';
-      const id = isString?option:option.id||option.caption;
-      const caption = isString?option:option.caption;
-      return <option key={id} value={id}>{caption}</option>;
+    const value = this.props.value;
+    const options = this.props.items.map(option => {
+      const isString = typeof option === 'string';
+      const id = isString ? option : option.id || option.caption;
+      const caption = isString ? option : option.caption;
+      if (value && value === id) {
+        return (
+          <option key={id || caption} value={id} selected={true}>
+            {caption}
+          </option>
+        );
+      }
+      return (
+        <option key={id || caption} value={id}>
+          {caption}
+        </option>
+      );
     });
+
     return (
       <select type="date" className="form-control" {...inputProps} ref="editor">
         {options}
       </select>
     );
   }
-};
+}
 
 module.exports = Select;
